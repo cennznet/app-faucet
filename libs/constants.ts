cdb040d52b40128d02c29b34846defde3f16fc39
Eug <eupston130@hotmail.com>
2022-03-07 16:35:09 -0700

    fixed Endowed account nonce and updated seed env vars

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,3 +1,7 @@
 export const CENNZNET_PUBLIC_API_URL: string = String(
-    process.env.NEXT_PUBLIC_CENNZNET_API_URL
+	process.env.NEXT_PUBLIC_CENNZNET_API_URL
 );
+
+export const ENDOWED_ACCOUNT_SEEDS: string[] = String(
+	process.env.ENDOWED_ACCOUNT_SEEDS
+).split(",");
--- pages/api/supplyAccount.ts
+++ pages/api/supplyAccount.ts
@@ -1,13 +1,26 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Api } from "@cennznet/api";
+import {
+	CENNZNET_PUBLIC_API_URL,
+	ENDOWED_ACCOUNT_SEEDS,
+} from "@/libs/constants";
+import { EndowedAccounts } from "@/libs/utils/EndowedAccounts";
 
 type Data = {
-  name: string
-}
+	success: boolean;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+	const api = await Api.create({ provider: CENNZNET_PUBLIC_API_URL });
+	const seeds = ENDOWED_ACCOUNT_SEEDS;
+	const keyType = "sr25519";
+	const endowedAccounts = new EndowedAccounts(api, seeds, keyType);
+	await endowedAccounts.init();
+	console.log(
+		`Available accounts number: ${endowedAccounts._availableAccounts.length}`
+	);
+	res.status(200).json({ success: true });
 }

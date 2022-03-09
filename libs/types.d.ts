ce44c5379931fa9a1b0e5aad07a06552c2b34d9b
Eugene Upston <35315320+eupston@users.noreply.github.com>
2022-03-08 17:17:11 -0700

    EndowedAccounts & Supply API Endpoint (#3)
    * initial endowed accounts class
    
    * fixed Endowed account nonce and updated seed env vars
    
    * removed old cennznet wallet keyring, updated supply endpoint with supplying given address and assetid
    
    * updated with connecting to different networks
    
    * debugging endowed seeds vercel
    
    * removed console info
    
    * fixed package deps and delete comment

---
+++ libs/types.d.ts
@@ -0,0 +1,1 @@
+export type CENNZNET_NETWORK = "nikau" | "rata" | "local";
---
+++ pages/api/supplyAccount.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Api } from "@cennznet/api";
+import {
+	CENNZNET_LOCAL_API_URL,
+	CENNZNET_NIKAU_API_URL,
+	CENNZNET_RATA_API_URL,
+	ENDOWED_ACCOUNT_SEEDS,
+	TRANSFER_AMOUNT,
+} from "@/libs/constants";
+import { EndowedAccounts } from "@/libs/utils/EndowedAccounts";
+import { CENNZNET_NETWORK } from "@/libs/types";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	try {
+		const body = req.body;
+		if (!body.assetId) throw new Error("assetId Param not provided!");
+		if (!body.address) throw new Error("address Param not provided!");
+		if (!body.network) throw new Error("network Param not provided!");
+		const { assetId, address, network } = body;
+		const cennznetNetwork: CENNZNET_NETWORK = network;
+		let networkUrl: string;
+		if (cennznetNetwork === "nikau") networkUrl = CENNZNET_NIKAU_API_URL;
+		else if (cennznetNetwork === "rata") networkUrl = CENNZNET_RATA_API_URL;
+		else if (cennznetNetwork === "local") networkUrl = CENNZNET_LOCAL_API_URL;
+		const api = await Api.create({ provider: networkUrl });
+		const endowedAccounts = new EndowedAccounts(api, ENDOWED_ACCOUNT_SEEDS);
+		await endowedAccounts.init();
+		const transfer = endowedAccounts.api.tx.genericAsset.transfer(
+			assetId,
+			address,
+			TRANSFER_AMOUNT
+		);
+		await endowedAccounts.send(transfer);
+		res.status(200).json({ success: true });
+	} catch (e) {
+		res.status(400).json({ success: false, error: e.message });
+	}
+}

2d396ab28be9f60961da126d98b38c7f9304df5e
aidan-starke <starke.aidan@gmail.com>
2022-03-16 13:10:21 +1300

    remove unused export

--- assets/vectors/index.ts
+++ assets/vectors/index.ts
@@ -3,4 +3,3 @@ export { default as CENNZ_BACKGROUND } from "@/assets/vectors/cennz_background.s
 export { default as CENNZnetBlue } from "@/assets/vectors/cennznet_blue.svg";
 export { default as CPAY } from "@/assets/vectors/cpay.svg";
 export { default as FAVICON } from "@/assets/vectors/favicon.svg";
-export { default as TWITTER } from "@/assets/vectors/twitter.svg";

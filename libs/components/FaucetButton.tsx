3331ded14c5869830244242c52ae6ab108beff2a
aidan-starke <starke.aidan@gmail.com>
2022-03-28 09:29:37 +1300

    connect metamask button

--- libs/components/FaucetButton.tsx
+++ libs/components/FaucetButton.tsx
@@ -1,16 +1,17 @@
 import { FC, MouseEventHandler } from "react";
 import { css } from "@emotion/react";
 import { signIn, useSession } from "next-auth/react";
+import { useMetaMaskWallet } from "@/libs/providers/MetaMaskWalletProvider";
+import { METAMASK } from "@/assets/vectors";
 
 const FaucetButton: FC<{
-	address: string;
-	network: string;
 	supplyAccount: MouseEventHandler<HTMLDivElement>;
-}> = ({ address, supplyAccount }) => {
+}> = ({ supplyAccount }) => {
 	const { data: session } = useSession();
+	const { connectWallet, selectedAccount } = useMetaMaskWallet();
 
-	if (session?.validAccount) {
-		if (address) {
+	if (selectedAccount) {
+		if (session?.validAccount) {
 			return (
 				<div css={styles.faucetButton} onClick={supplyAccount}>
 					<p>SEND TOKENS</p>
@@ -19,18 +20,22 @@ const FaucetButton: FC<{
 		}
 
 		return (
-			<div css={styles.faucetButton}>
-				<p>PLEASE ENTER A CENNZnet ADDRESS</p>
+			<div
+				css={styles.faucetButton}
+				onClick={async () => await signIn("twitter")}
+			>
+				<p>PLEASE SIGN IN WITH A VALID TWITTER ACCOUNT</p>
 			</div>
 		);
 	}
 
 	return (
 		<div
-			css={styles.faucetButton}
-			onClick={async () => await signIn("twitter")}
+			css={styles.metamaskButton}
+			onClick={async () => await connectWallet?.()}
 		>
-			<p>PLEASE SIGN IN WITH A VALID TWITTER ACCOUNT</p>
+			<img src={METAMASK} alt="MetaMask Logo" css={styles.brandLogo} />
+			<p>CONNECT METAMASK</p>
 		</div>
 	);
 };
@@ -45,8 +50,9 @@ export const styles = {
 		margin: 15px 0;
 		text-align: center;
 		border-radius: 4px;
-		background-color: ${palette.primary.main};
-		color: white;
+		background-color: white;
+		color: ${palette.primary.main};
+		border: 1px solid ${palette.primary.main};
 		letter-spacing: 0.5px;
 		justify-content: center;
 		align-items: center;
@@ -61,10 +67,43 @@ export const styles = {
 		}
 
 		&:hover {
-			background-color: white;
-			color: ${palette.primary.main};
-			border: 1px solid ${palette.primary.main};
+			background-color: ${palette.primary.main};
+			color: white;
 			transition-duration: 0.3s;
 		}
+	`,
+
+	metamaskButton: css`
+		cursor: pointer;
+		width: 100%;
+		height: 40px;
+		margin: 15px 0;
+		text-align: center;
+		border-radius: 4px;
+		border: 1px solid #e2761b;
+		color: #e2761b;
+		letter-spacing: 0.5px;
+		justify-content: center;
+		align-items: center;
+		display: flex;
+		font-weight: bold;
+
+		p {
+			font-size: 14px;
+			@media (max-width: 500px) {
+				font-size: 10px;
+			}
+		}
+
+		&:hover {
+			background-color: #e2761b;
+			color: white;
+			transition-duration: 0.3s;
+		}
+	`,
+
+	brandLogo: css`
+		width: 28px;
+		margin-right: 0.5em;
 	`,
 };

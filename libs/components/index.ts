af8f6be3e56d9ac18fed603c0187e524f0d50f59
aidan-starke <starke.aidan@gmail.com>
2022-03-28 09:51:52 +1300

    delete `NetworkSelect`

--- libs/components/index.ts
+++ libs/components/index.ts
@@ -5,6 +5,5 @@ export { default as MetaMaskAccount } from "@/libs/components/MetaMaskAccount";
 export { default as FaucetButton } from "@/libs/components/FaucetButton";
 export { default as FaucetDetails } from "@/libs/components/FaucetDetails";
 export { default as FaucetProgress } from "@/libs/components/FaucetProgress";
-export { default as NetworkSelect } from "@/libs/components/NetworkSelect";
 export { default as SignOut } from "@/libs/components/SignOut";
 export { default as TokenSelect } from "@/libs/components/TokenSelect";

f37698b98c180fa168e69d1e745c4f04f0e91d9d
aidan-starke <starke.aidan@gmail.com>
2022-03-29 08:28:54 +1300

    add CENNZ token if not already there for selectedAccount

--- libs/components/Faucet.tsx
+++ libs/components/Faucet.tsx
@@ -6,6 +6,7 @@ import { CENNZNetNetwork, CENNZnetToken, TxStatus } from "@/libs/types";
 import { SUPPORTED_TOKENS } from "@/libs/constants";
 import {
 	addCENNZnetToMetaMask,
+	addCENNZTokenToMetaMask,
 	ensureEthereumChain,
 	supplyAccount,
 } from "@/libs/utils";
@@ -47,7 +48,9 @@ const Faucet: FC = () => {
 	};
 
 	const fetchSupplyResponse = useCallback(async () => {
-		if (!selectedAccount || !network) return;
+		if (!selectedAccount || !token || !network) return;
+		if (token.symbol === "CENNZ") await addCENNZTokenToMetaMask();
+
 		setResponse({
 			message: `Retrieving ${token.symbol} from the Faucet`,
 			status: "in-progress",

d8c84e2f9d615784698b820ed1dff8e5c2b9dac1
aidan-starke <starke.aidan@gmail.com>
2022-04-29 05:56:59 +1200

    disable button & display `NETWORK SWITCHING` if network switching

--- libs/components/FaucetButton.tsx
+++ libs/components/FaucetButton.tsx
@@ -1,14 +1,23 @@
-import { useEffect, useState, VFC } from "react";
+import { useEffect, useMemo, useState, VFC } from "react";
 import { css } from "@emotion/react";
 import NewWindow from "react-new-window";
 import { useSession } from "next-auth/react";
 import { Theme } from "@mui/material";
 import { useFaucet } from "@/libs/providers/FaucetProvider";
+import { isCorrectApiProvider } from "@/libs/utils";
+import { useCENNZApi } from "@/libs/providers/CENNZApiProvider";
 
 const FaucetButton: VFC = () => {
+	const { api } = useCENNZApi();
+	const { network } = useFaucet();
 	const { data: session } = useSession();
 	const [popup, setPopup] = useState<boolean>(false);
 	const [warned, setWarned] = useState<boolean>(false);
+
+	const disabled = useMemo(
+		() => !isCorrectApiProvider(api, network),
+		[api, network]
+	);
 
 	useEffect(() => {
 		if (!session || warned) return;
@@ -25,8 +34,12 @@ const FaucetButton: VFC = () => {
 
 	if (session?.validAccount) {
 		return (
-			<button css={styles.root(false)} type="submit">
-				SEND TOKENS
+			<button
+				css={styles.root(disabled)}
+				type="submit"
+				disabled={disabled}
+			>
+				{disabled ? "NETWORK SWITCHING" : "SEND TOKENS"}
 			</button>
 		);
 	}
@@ -40,11 +53,7 @@ const FaucetButton: VFC = () => {
 					features={{ height: 600, width: 800 }}
 				/>
 			)}
-			<button
-				css={styles.root(false)}
-				type="button"
-				onClick={() => setPopup(true)}
-			>
+			<button css={styles.root} type="button" onClick={() => setPopup(true)}>
 				SIGN IN WITH TWITTER
 			</button>
 		</>
@@ -55,20 +64,20 @@ export default FaucetButton;
 
 const styles = {
 	root:
-		(metaMask?: boolean) =>
+		(disabled?: boolean) =>
 		({ palette, transitions }: Theme) =>
 			css`
-				cursor: pointer;
+				cursor: ${disabled ? "not-allowed" : "pointer"};
 				text-align: center;
 				border-radius: 4px;
 				background-color: white;
-				color: ${metaMask ? "#e2761b" : palette.primary.main};
+				color: ${disabled ? palette.text.disabled : palette.primary.main};
 				font-weight: bold;
-				border: 1px solid ${metaMask ? "#e2761b" : palette.primary.main};
+				border: 1px solid ${palette.primary.main};
 				transition: background-color ${transitions.duration.short}ms;
 				display: block;
 				margin: 0 auto;
-				padding: ${metaMask ? "0.5em 0.75em" : "1em 1.5em"};
+				padding: 1em 1.5em;
 
 				span {
 					display: inline-flex;
@@ -80,8 +89,8 @@ const styles = {
 				}
 
 				&:hover {
-					background-color: ${metaMask ? "#e2761b" : palette.primary.main};
-					color: white;
+					background-color: ${disabled ? "white" : palette.primary.main};
+					color: ${disabled ? palette.text.disabled : "white"};
 				}
 			`,
 };

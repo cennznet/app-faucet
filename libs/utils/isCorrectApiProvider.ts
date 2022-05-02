9bd72bcc711e8682f564314d2c23512db9173599
aidan-starke <starke.aidan@gmail.com>
2022-05-03 07:30:33 +1200

    remove unused filed

--- libs/utils/isCorrectApiProvider.ts
+++
@@ -1,9 +0,0 @@
-import { Api } from "@cennznet/api";
-import { CENNZnetNetwork } from "@/libs/types";
-
-export default function isCorrectApiProvider(
-	api: Api,
-	network: CENNZnetNetwork
-): boolean {
-	return api?.runtimeChain.toString().includes(network);
-}

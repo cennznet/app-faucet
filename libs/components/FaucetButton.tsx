0f1b3ec9a56ed1fdf210d53352a0afc6227dd471
aidan-starke <starke.aidan@gmail.com>
2022-05-03 07:30:19 +1200

    don't disable button

--- libs/components/FaucetButton.tsx
+++ libs/components/FaucetButton.tsx
@@ -1,23 +1,13 @@
-import { useEffect, useMemo, useState, VFC } from "react";
+import { useEffect, useState, VFC } from "react";
 import { css } from "@emotion/react";
 import NewWindow from "react-new-window";
 import { useSession } from "next-auth/react";
 import { Theme } from "@mui/material";
-import { useFaucet } from "@/libs/providers/FaucetProvider";
-import { isCorrectApiProvider } from "@/libs/utils";
-import { useCENNZApi } from "@/libs/providers/CENNZApiProvider";
 
 const FaucetButton: VFC = () => {
-	const { api } = useCENNZApi();
-	const { network } = useFaucet();
 	const { data: session } = useSession();
 	const [popup, setPopup] = useState<boolean>(false);
 	const [warned, setWarned] = useState<boolean>(false);
-
-	const disabled = useMemo(
-		() => !isCorrectApiProvider(api, network),
-		[api, network]
-	);
 
 	useEffect(() => {
 		if (!session || warned) return;
@@ -34,12 +24,8 @@ const FaucetButton: VFC = () => {
 
 	if (session?.validAccount) {
 		return (
-			<button
-				css={styles.root(disabled)}
-				type="submit"
-				disabled={disabled}
-			>
-				{disabled ? "NETWORK SWITCHING" : "SEND TOKENS"}
+			<button css={styles.root} type="submit">
+				SEND TOKENS
 			</button>
 		);
 	}
@@ -63,34 +49,32 @@ const FaucetButton: VFC = () => {
 export default FaucetButton;
 
 const styles = {
-	root:
-		(disabled?: boolean) =>
-		({ palette, transitions }: Theme) =>
-			css`
-				cursor: ${disabled ? "not-allowed" : "pointer"};
-				text-align: center;
-				border-radius: 4px;
-				background-color: white;
-				color: ${disabled ? palette.text.disabled : palette.primary.main};
-				font-weight: bold;
-				border: 1px solid ${palette.primary.main};
-				transition: background-color ${transitions.duration.short}ms;
-				display: block;
-				margin: 0 auto;
-				padding: 1em 1.5em;
+	root: ({ palette, transitions }: Theme) =>
+		css`
+			cursor: pointer;
+			text-align: center;
+			border-radius: 4px;
+			background-color: white;
+			color: ${palette.primary.main};
+			font-weight: bold;
+			border: 1px solid ${palette.primary.main};
+			transition: background-color ${transitions.duration.short}ms;
+			display: block;
+			margin: 0 auto;
+			padding: 1em 1.5em;
 
-				span {
-					display: inline-flex;
-				}
+			span {
+				display: inline-flex;
+			}
 
-				img {
-					height: 2em;
-					margin: 0.65em 0.2em;
-				}
+			img {
+				height: 2em;
+				margin: 0.65em 0.2em;
+			}
 
-				&:hover {
-					background-color: ${disabled ? "white" : palette.primary.main};
-					color: ${disabled ? palette.text.disabled : "white"};
-				}
-			`,
+			&:hover {
+				background-color: ${palette.primary.main};
+				color: white;
+			}
+		`,
 };

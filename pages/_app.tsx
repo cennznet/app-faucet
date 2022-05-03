d0c005af49c99fccab1c803882e6b2874893719e
aidan-starke <starke.aidan@gmail.com>
2022-05-04 09:31:16 +1200

    remove unused code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,6 @@ import FaucetProvider from "@/libs/providers/FaucetProvider";
 import { useRouter } from "next/router";
 import { trackPageView } from "@/libs/utils";
 import { useEffect } from "react";
-import CENNZApiProvider from "@/libs/providers/CENNZApiProvider";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	const { events } = useRouter();
